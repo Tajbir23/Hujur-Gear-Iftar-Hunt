34ba227589040a7ab89603a3227ec7a9da31f9f9
Tajbir23 <studenttajbirislam@gmail.com>
2026-02-22 19:53:44 +0600

    feat: add Google Analytics G-F95VCK2ZVR

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 
@@ -119,6 +120,20 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F95VCK2ZVR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F95VCK2ZVR');
+          `}
+        </Script>
       </body>
     </html>
   );
